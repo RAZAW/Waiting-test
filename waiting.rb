49fcf872db96f4fc983512f76b3bda193252be1f
Waqas Ali Raza <syedwaqasaliraza@gmail.com>
2017-01-30 15:47:04 +0000

    adding explicit wait

--- waiting.rb
+++ waiting.rb
@@ -1,23 +1,19 @@
 #filename: waiting.rb
 
+#filename: waiting.rb
+
 require 'selenium-webdriver'
 require 'rspec/expectations'
 include RSpec::Matchers
 
 def setup
 	@driver = Selenium::WebDriver.for :firefox
-	#implicit wait
-	@driver.manage.timeouts.implicit_wait = 3
+	#@driver.manage.timeouts.implicit_wait = 3
 end
 
 def teardown
 	@driver.quit
 end
-#explicit wait
-def wait_for(seconds)
-	Selenium::WebDriver::Wait.new(timeout: seconds).until { yield }
-end
-
 
 def run
 	setup
@@ -25,14 +21,25 @@ def run
 	teardown
 end
 
+def wait_for(seconds)
+	Selenium::WebDriver::Wait.new(timeout: seconds).until {yield}
+end
+
 
 run do
-
-@driver.get 'http://the-internet.herokuapp.com/dynamic_loading/2'
-@driver.find_element(css: '#start button').click
-wait_for(10) { @driver.find_element(css: '#finish').displayed? }
-expect(@driver.find_element(css: '#finish').text).to eql('Hello World!')
-
-
+	@driver.get 'http://the-internet.herokuapp.com/dynamic_loading/1'
+	@driver.find_element(css: '#start button').click
+	wait_for(10) { @driver.find_element(css: '#finish').displayed? }
+	expect(@driver.find_element(css: '#finish').text).to eql ('Hello World!')
 end
+
+run do
+	@driver.get 'http://the-internet.herokuapp.com/dynamic_loading/2'
+	@driver.find_element(css: '#start button').click
+	wait_for(10) { @driver.find_element(css: '#finish').displayed? }
+	expect(@driver.find_element(css: '#finish').text).to eql ('Hello World!')
+end
+
+
+
 
